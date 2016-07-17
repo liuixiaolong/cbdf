02b17c88e3017493e4ab0cc8befbe810b8ef428b
happylonglong <happylonglong@Lenovo-PC>
2016-07-17 17:16:09 +0800

    ssd

---
+++ data/config.db.php
@@ -0,0 +1,12 @@
+<?php 
+    
+$db_config["hostname"] = "mysql.sql93.cdncenter.net"; //服务器地址
+$db_config["username"] = "sq_wapmedia02"; //数据库用户名
+$db_config["password"] = "wapmedia"; //数据库密码
+$db_config["database"] = "sq_wapmedia02"; //数据库名称
+$db_config["charset"] = "utf8";//数据库编码
+$db_config["pconnect"] = 1;//开启持久连接
+$db_config["log"] = 0;//开启日志
+$db_config["logfilepath"] = 'dblog/';//开启日志
+
+?>
